8f23d290d9ef94a1e77b1853b9353df8c639f776
yuvi-04 <114637709+yuvi-04@users.noreply.github.com>
2022-11-15 13:31:49 +0530

    Add files via upload

--- avlTree.cpp
+++ avlTree.cpp
@@ -71,6 +71,77 @@ public:
     }
     void insert(int x)
     {
-
+		Node* temp = new Node(x);
+		if(root == NULL)
+		{
+			root = temp;
+			return;
+		}
+		Node* p = root, *q;
+		while(p != NULL)
+		{
+			if(x < p->info)
+			{
+				q = p;
+				p = p->left;
+			}
+			else
+			{
+				q = p;
+				p = p->right;
+			}
+		}
+		if(x < q->info)
+			q->left = temp;
+		else
+			q->right = temp;
+			
+		temp->height = 1 + maxNum(height(temp->left),height(temp->right));
+		
+		int bal = getBalance(temp);
+		
+		//ll case
+		if(bal > 1 && x < root->left->info)
+			root = rightRotate(root);
+		
+		//rr case
+		if(bal < -1 && x > root->right->info)
+			root = leftRotate(root);
+		
+		//lr case
+		if(bal > 1 && x > root->left->info)
+		{
+			root->left = leftRotate(root->left);
+			root = rightRotate(root);
+		}
+		
+		//rl case
+		if(bal < -1 && x < root->right->info)
+		{
+			root->right = rightRotate(root->right);
+			root = leftRotate(root);
+		}
     }
+    void preOrder(Node* p)
+    {
+    	if(p == NULL)
+    		return;
+    	cout<<p->info<<" ";
+    	preOrder(p->left);
+    	preOrder(p->right);
+	}
 };
+int main()
+{
+	AVLtree tree;
+	tree.insert(10);
+	tree.insert(20);
+	tree.insert(30);
+	tree.insert(40);
+	tree.insert(50);
+	tree.insert(25);
+	
+	Node* root = tree.getRoot();
+	
+	tree.preOrder(root);
+}
